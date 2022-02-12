ee4721b83c45b7cf9a9fa8607d35e2b99ef77823
ganler <jaway.liu@gmail.com>
2022-02-12 05:42:18 -0600

    pure cmake testing and compilation

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -6,6 +6,19 @@ from lit.llvm import llvm_config
 from lit.llvm.subst import ToolSubst
 
 config.name = 'lit-SROA-jiawei6'
-config.test_format = lit.formats.ShTest(True)
+config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 config.suffixes = ['.c']
 config.test_source_root = os.path.dirname(__file__)
+
+if platform.system() == 'Darwin':
+    tool_substitutions = [ToolSubst('%clang', 'clang', extra_args=["-isysroot", "`xcrun --show-sdk-path`", "-mlinker-version=0"])]
+else:
+    tool_substitutions = [ToolSubst('%clang', 'clang',)]
+llvm_config.add_tool_substitutions(tool_substitutions)
+
+tools = ["opt", "lli", "not", "FileCheck", "clang"]
+llvm_config.add_tool_substitutions(tools, config.llvm_tools_dir)
+
+config.substitutions.append(('%shlibext', config.llvm_shlib_ext))
+config.substitutions.append(('%testdir', config.test_source_root))
+config.substitutions.append(('%builddir', config.llvm_build_dir))
--- tests/suite/sroa-1.c
+++ tests/suite/sroa-1.c
@@ -1,6 +1,6 @@
 // RUN: clang %s -S -emit-llvm -o %t.ll
-// RUN: opt -enable-new-pm=0 -load ../../build/SROA/SROAPass.so --scalarrepl-jiawei6 %t.ll -S -o %t.out.ll
-// RUN: BEFORE=%t.ll AFTER=%t.out.ll python3 -m unittest property_test.TestSORA.test_no_struct
+// RUN: opt -load %builddir/SROA/SROAPass%shlibext -scalarrepl-jiawei6 %t.ll -S -o %t.out.ll
+// RUN: BEFORE=%t.ll AFTER=%t.out.ll python3 %testdir/property_test.py TestSORA.test_no_struct
 
 struct Ticket {
     int price_per_mile;
