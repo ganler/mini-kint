3a512fbf5250f7aedc52af1a601af8f4023bcad9
ganler <jaway.liu@gmail.com>
2022-02-12 15:23:09 -0600

    project1 submission form

--- tests/suite/sroa-1.c
+++ tests/suite/sroa-1.c
@@ -1,5 +1,5 @@
 // RUN: clang %s -O0 -S -emit-llvm -o %t.ll
-// RUN: opt -load %builddir/SROA/SROAPass%shlibext -scalarrepl-jiawei6 %t.ll -S -o %t.out.ll
+// RUN: opt -load %builddir/pass/SROAPass%shlibext -scalarrepl-jiawei6 %t.ll -S -o %t.out.ll
 // RUN: BEFORE=%t.ll AFTER=%t.out.ll python3 %testdir/property_test.py TestSORA.test_no_struct
 
 struct Ticket {
